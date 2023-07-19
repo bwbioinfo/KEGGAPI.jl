0e067099c967485b0659d88e2eeec2206f25748c
Nicholas <nicholas.geoffrion@me.com>
2023-07-19 18:53:12 -0400

    Added basic tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,20 @@
 using KEGGAPI
 using Test
+using DataFrames
 
 @testset "KEGGAPI.jl" begin
-    info = KEGGAPI.info("kegg")
-    @test typeof(info) == String
+    # Retrieve information about the KEGG database
+    kegg_info = KEGGAPI.info("kegg")
+    @test isa(kegg_info, String)  # Check if the retrieved info is a string
+    @test length(kegg_info) > 0  # Check if the retrieved info is not empty
+    
+    # Retrieve a list of KEGG pathways
+    kegg_pathways = KEGGAPI.list("pathway")
+    @test isa(kegg_pathways, DataFrame)  # Check if the pathways data is a DataFrame
+    @test size(kegg_pathways) > (0, 0)  # Check if the pathways data is not empty (has rows and columns)
+    
+    # Search for information about a specific compound (glucose)
+    glucose = KEGGAPI.find("compound", "glucose")
+    @test isa(glucose, DataFrame)  # Check if the glucose data is a DataFrame
+    @test size(glucose) > (0, 0)  # Check if the glucose data is not empty (has rows and columns)
 end
