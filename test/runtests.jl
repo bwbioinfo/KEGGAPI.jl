5b452ef964c79437265caa06f762d5739a90dfde
Nicholas <nicholas.geoffrion@me.com>
2023-07-24 15:27:33 -0400

    Added test for list.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,16 @@
 using KEGGAPI
 using Test
 
-@testset "KEGGAPI.jl" begin
+@testset "KEGGAPI.info" begin
     # Retrieve information about the KEGG database
     kegg_info = KEGGAPI.info("kegg")
     @test isa(kegg_info, String)  # Check if the retrieved info is a string
     @test length(kegg_info) > 0  # Check if the retrieved info is not empty
     @test_throws KEGGAPI.RequestError KEGGAPI.info("fail")
-    
+end
+
+@testset "KEGGAPI.list" begin
     # Retrieve a list of KEGG pathways
     kegg_pathways = KEGGAPI.list("pathway")
-    
-    # Search for information about a specific compound (glucose)
-    glucose = KEGGAPI.find("compound", "glucose")
+    @test isa(kegg_pathways, KEGGAPI.KeggPathwayList)  # Check if the retrieved info is a string
 end
