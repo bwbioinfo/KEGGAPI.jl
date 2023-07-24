c0ad111d166502b2de8bae5d78a970f48bf5ac7a
Nicholas <nicholas.geoffrion@me.com>
2023-07-24 14:26:10 -0400

    Added some custom structures.

---
+++ src/Structures.jl
@@ -0,0 +1,18 @@
+## File to hold the types of the KEGG API
+
+# This type is used to represent a KEGG API request error.
+struct RequestError <: Exception
+    message::String
+end
+
+mutable struct KeggPathwayList
+    url::String
+    colnames:: Vector{String}
+    data::Vector{Any}
+end
+
+mutable struct KeggOrganismList
+    url::String
+    colnames:: Vector{String}
+    data::Vector{Any}
+end
