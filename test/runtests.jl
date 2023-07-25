48b8a0d07370985289a9dc18d6c14ba9194a565b
mvapontes <mvaguilarpontes@gmail.com>
2023-07-25 16:37:40 +0200

    add test function image

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,4 +13,23 @@ end
     # Retrieve a list of KEGG pathways
     kegg_pathways = KEGGAPI.list("pathway")
     @test isa(kegg_pathways, KEGGAPI.KeggPathwayList)  # Check if the retrieved info is a string
+    @test length(kegg_pathways) > 0  # Check if the retrieved list is not empty	
+    @test_throws KEGGAPI.ResquestError KEGG.list("fail")
+end
+
+@testset "KEGGAPI.get_image" begin
+    # Retrieve an image of a specific pathway from the KEGG API.
+    kegg_image = KEGGAPI.get_image("hsa00010")
+    @test isa(kegg_image, Vector)  # Check if the retrieve info is a vector
+    @test length(kegg_image) > 0  # Check if the retreived vector is not empty
+    @test_throws KEGGAPI.RequestError KEGGAPI.get_image("fail")
+end
+
+@testset "KEGGAPI.save_image"begin
+    # Save an image to a file
+    image = KEGGAPI.get_image("hsa00010")
+    save_image = KEGGAPI.save_image(image, "image.png")
+    @test isa(save_image, Vector) #  Check if the retrieved info is a vector
+    @test length(save_image) > 0  # Check if the retrieved vector is not empty
+    @test_throws KEGGAPI.RequestError KEGGAPI.save_image("fail")
 end
