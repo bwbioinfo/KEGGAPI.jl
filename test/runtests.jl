24fa703ef9aeb1cc9f4f5bc8788463392df099da
mvapontes <mvaguilarpontes@gmail.com>
2023-07-26 20:27:38 +0200

    update length list test

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,7 +13,7 @@ end
     # Retrieve a list of KEGG pathways
     kegg_pathways = KEGGAPI.list("pathway")
     @test isa(kegg_pathways, KEGGAPI.KeggPathwayList)  # Check if the retrieved info is a string
-    @test length(kegg_pathways) > 0  # Check if the retrieved list is not empty	
+    @test length(kegg_pathways) > (0, 0)  # Check if the retrieved list is not empty	
     @test_throws KEGGAPI.ResquestError KEGGAPI.list("fail")
 end
 
